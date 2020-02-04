78e06669c4be0a6e5e6ca8ce8935fd554f416f3a
Evan Ristow <evan.ristow1@gmail.com>
2020-02-04 15:21:38 -0500

    Pass prod uri through cmd line arg rather than new .properties file.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -Dspring.config.location=src/resources/application-prod.properties -jar target/WebDaw-0.0.1-SNAPSHOT.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -Dspring.config.location=src/resources/application-prod.properties -Dspring.data.mongodb.uri=mongodb://heroku_p4g5zs1h:ql5sjnmbhl3jh4goukjlafq604@ds151007.mlab.com:51007/heroku_p4g5zs1h?retryWrites=false -jar target/WebDaw-0.0.1-SNAPSHOT.jar
--- src/main/java/com/seniorproj/WebDaw/WebDawApplication.java
+++ src/main/java/com/seniorproj/WebDaw/WebDawApplication.java
@@ -1,13 +1,13 @@
 package com.seniorproj.WebDaw;
 
-import com.seniorproj.WebDaw.repo.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+// import com.seniorproj.WebDaw.repo.UserRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebDawApplication{
+public class WebDawApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebDawApplication.class, args);
 	}
